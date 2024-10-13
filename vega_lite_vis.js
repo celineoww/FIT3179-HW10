a8960a40a8be57d4be4d05b9adcd50206266dbee
celineee <cwon0113@student.Monash.edu>
2024-10-13 22:16:50 +0800

    updated necessary files for hw10

---
+++ vega_lite_vis.js
@@ -0,0 +1,11 @@
+var vg_1 = "Map1.json";
+var vg_2 = "BumpChart.json";
+
+vegaEmbed("#choropleth_map", vg_1).then(function(result) {
+  
+}).catch(console.error);
+
+
+vegaEmbed("#bump_chart", vg_2).then(function(result) {
+  
+}).catch(console.error);
